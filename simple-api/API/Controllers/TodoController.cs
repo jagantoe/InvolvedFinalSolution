b2178c2117fe0b66da645e425da10c68cc3a7af3
Koen Seeuws <168718548+koen-seeuws@users.noreply.github.com>
2024-10-26 13:10:52 +0200

    Simple TODO Api

---
+++ simple-api/API/Controllers/TodoController.cs
@@ -0,0 +1,118 @@
+using API.Database;
+using API.DTOs;
+using API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoDbContext _todoDbContext;
+
+        public TodoController(TodoDbContext todoDbContext) {
+            _todoDbContext = todoDbContext;
+            todoDbContext.Database.EnsureCreated();
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult Add([FromBody] TodoDto todoDto)
+        {
+            var todo = new Todo() {
+                Id = todoDto.Id,
+                Title = todoDto.Title,
+                Assignee = todoDto.Assignee
+            };
+
+            _todoDbContext.Todos.Add(todo);
+            _todoDbContext.SaveChanges();
+
+            return Ok(todo.Id);
+        }
+
+        [HttpGet("[action]/{id}")]
+        public IActionResult Get([FromRoute] int id)
+        {
+            var todo = _todoDbContext.Todos.Find(id);
+
+            if (todo == null) return NotFound();
+
+            var todoDto = new TodoDto(){
+                Id = todo.Id,
+                Title = todo.Title,
+                Assignee = todo.Assignee
+            };
+
+            return Ok(todoDto);
+        }
+    
+        [HttpGet("[action]")]
+        public IActionResult GetAll()
+        {
+            var todoDtos = _todoDbContext.Todos
+                .Select(todo => new TodoDto(){
+                    Id = todo.Id,
+                    Title = todo.Title,
+                    Assignee = todo.Assignee
+                })
+                .ToList();
+
+            return Ok(todoDtos);
+        }
+        
+        [HttpGet("[action]")]
+        public IActionResult Search([FromQuery] string? title, [FromQuery] string? assignee)
+        {
+            var todos = _todoDbContext.Todos.AsQueryable();
+
+            if(!string.IsNullOrWhiteSpace(title))
+                todos = todos.Where(todo => todo.Title.Contains(title));
+
+            if(!string.IsNullOrWhiteSpace(assignee))
+                todos = todos.Where(todo => todo.Title.Contains(assignee));
+
+            var todoDtos = todos
+                .Select(todo => new TodoDto(){
+                    Id = todo.Id,
+                    Title = todo.Title,
+                    Assignee = todo.Assignee
+                })
+                .ToList();
+
+            return Ok(todoDtos);
+        }
+    
+        
+        [HttpPut("[action]")]
+        public IActionResult Update([FromBody] TodoDto todoDto)
+        {
+            var todo = _todoDbContext.Todos.Find(todoDto.Id);
+
+            if(todo == null) return NotFound();
+
+            todo.Title = todoDto.Title;
+            todo.Assignee = todoDto.Assignee;
+            todo.Description = todoDto.Description;
+
+            _todoDbContext.Todos.Update(todo);
+            _todoDbContext.SaveChanges();
+
+            return Ok();
+        }
+        
+        [HttpDelete("[action]/{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var todo = _todoDbContext.Todos.Find(id);
+
+            if(todo == null) return NotFound();
+
+            _todoDbContext.Todos.Remove(todo);
+            _todoDbContext.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ simple-api/API/Database/TodoDbContext.cs
@@ -0,0 +1,10 @@
+using System;
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Database;
+
+public class TodoDbContext(DbContextOptions<TodoDbContext> options) : DbContext(options)
+{
+    public DbSet<Todo> Todos {get; set;}
+}
